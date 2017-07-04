167862eb788d7395dc3c4419bfd76a971d6984a4
Shalom Rachapudi <Shalom Rachapudi>
2017-07-04 22:42:27 +0100

    Set _WIN32_WINNT to Windows 10 (0x0A00) in targetver.h

--- Backend/targetver.h
+++ Backend/targetver.h
@@ -9,6 +9,8 @@
 // set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 
 #if defined PREDEF_OS_WINDOWS
+#include <WinSDKVer.h>
+#define _WIN32_WINNT 0x0A00
 #include <SDKDDKVer.h>
 #endif
 
